e2e341bd2609892178fe6cc672278d7a2148562f
澁谷悠大 <univer405@gmail.com>
2022-04-28 14:02:04 +0900

    Initial Commit

---
+++ SHISAKU/SHISAKUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHISAKUApp.swift
+//  SHISAKU
+//
+//  Created by 澁谷悠大 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SHISAKUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
